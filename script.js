6ef43d9d508e211fac0482895b492f37ce38a8e8
planutius <37017701+planutius@users.noreply.github.com>
2018-04-18 23:51:37 +0200

    Inicio con modal de progreso
    Modal que muestra el progreso de la web al cargar la web

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
 
-function progreso (){
- var porcentaje = 0;
- for (porcentaje=0;porcentaje<25;porcentaje++){
-    $('progress').attr('value',porcentaje);
-    + porcentaje;
- }
+function avisoProgreso (){
+ $('#avisoProgreso').modal('show');
  
 };
-
-/*$('.option').hover(
-    function(){
-        $('.opcion-icono').css(
-            {fontsize: 46 px;
-            transition: 0.3s;}
-        );
-    }
-);*/
